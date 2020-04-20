3658b548890610ead4f2c0db9e21856731b44dac
Daniil_Sapielkin <vipdanik98@gmail.com>
2020-04-20 12:48:46 +0300

    not work api kay

--- bigbrain/src/container/MainContainer.jsx
+++ bigbrain/src/container/MainContainer.jsx
@@ -1,17 +1,19 @@
 import React, { Component } from "react";
 
 import { ButtonPlus } from "../components/Button/ButtonMainPlus";
-import { MapContainer } from "./MapContainer";
+import { MyMapComponent } from "./MapContainer";
+
+// export default GoogleApiWrapper({
+//   apiKey: "AIzaSyCynkUuNtj3PZVtBa270xT81U2jP8H3rkk",
+// })(MapContainer);
 
 export class MainContainer extends Component {
-  google = "https://api.google.com/some/script.js";
-
   render() {
     return (
       <div className="main">
-        <MapContainer google={this.google} />
+        <MyMapComponent />
 
-<ButtonPlus />
+        {/* <ButtonPlus /> */}
       </div>
     );
   }
--- bigbrain/src/container/MapContainer.jsx
+++ bigbrain/src/container/MapContainer.jsx
@@ -1,59 +1,136 @@
+// import React, { Component } from "react";
+
+// // import { GoogleMap } from "../components/MainMap/MainMap";
+
+// // import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+
+// import { Map, Marker, GoogleApiWrapper } from "react-google-maps";
+
+// import { connect } from "react-redux";
+
+// const mapStyles = {
+//   width: "100%",
+//   height: "100%",
+// };
+
+// class MapContainer extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       stores: [
+//         {
+//           lat: 50.025019,
+//           lng: 36.228515,
+//         },
+//       ],
+//     };
+//     console.log(props);
+//   }
+
+//   displayMarkers = () => {
+//     return this.state.stores.map((store, index) => {
+//       return (
+//         <Marker
+//           key={index}
+//           id={index}
+//           position={{
+//             lat: store.latitude,
+//             lng: store.longitude,
+//           }}
+//           onClick={() => console.log("You clicked me!")}
+//         />
+//       );
+//     });
+//   };
+//   render() {
+//     return (
+//       <Map
+//         google={this.props.google}
+//         zoom={8}
+//         style={mapStyles}
+//         initialCenter={{ lat: 49.989, lng: 36.235 }}
+//       >
+//         <Marker position={{ lat: 49.989, lng: 36.235 }} /> || "Временно не
+//         доступно"
+//       </Map>
+//     );
+//   }
+// }
+
+// export default connect(null)(
+//   GoogleApiWrapper({
+//     apiKey: "",
+//   })(MapContainer)
+// );
+
+// export default GoogleApiWrapper({
+//   apiKey: "AIzaSyCynkUuNtj3PZVtBa270xT81U2jP8H3rkk",
+// })(MapContainer);
+
 import React, { Component } from "react";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from "react-google-maps";
 
-// import { GoogleMap } from "../components/MainMap/MainMap";
+import { compose, withProps } from "recompose";
 
-import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+// const DocsApiKeyInput = require("./DocsApiKeyInput").default;
 
-const mapStyles = {
-  width: "100%",
-  height: "100%",
-};
-
-export class MapContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      stores: [
-        {
-          lat: 50.025019,
-          lng: 36.228515,
-        },
-      ],
-    };
-  }
-
-  displayMarkers = () => {
-    return this.state.stores.map((store, index) => {
-      return (
-        <Marker
-          key={index}
-          id={index}
-          position={{
-            lat: store.latitude,
-            lng: store.longitude,
-          }}
-          onClick={() => console.log("You clicked me!")}
-        />
-      );
-    });
-  };
-  render() {
-    console.log(this.props.google);
-
-    return (
-      <Map
-        google={"https://api.google.com/some/script.js"}
-        zoom={8}
-        style={mapStyles}
-        initialCenter={{ lat: 49.989, lng: 36.235 }}
-      >
-        <Marker position={{ lat: 49.989, lng: 36.235 }} /> || "Временно не
-        доступно"
-      </Map>
-    );
-  }
+{
+  /* <DocsApiKeyInput />; */
 }
 
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyCynkUuNtj3PZVtBa270xT81U2jP8H3rkk",
-})(MapContainer);
+export const MyMapComponent = compose(
+  withProps({
+    googleMapURL:
+      "https://maps.googleapis.com/maps/api/js?=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `100vh` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) => (
+  <GoogleMap defaultZoom={8} defaultCenter={{ lat: 49.990159, lng: 36.233081 }}>
+    {props.isMarkerShown && (
+      <Marker
+        zoom="8"
+        position={{ lat: -34.397, lng: 150.644 }}
+        onClick={props.onMarkerClick}
+      />
+    )}
+  </GoogleMap>
+));
+
+// class MyFancyComponent extends React.PureComponent {
+//   state = {
+//     isMarkerShown: false,
+//   }
+
+//   componentDidMount() {
+//     this.delayedShowMarker()
+//   }
+
+//   delayedShowMarker = () => {
+//     setTimeout(() => {
+//       this.setState({ isMarkerShown: true })
+//     }, 3000)
+//   }
+
+//   handleMarkerClick = () => {
+//     this.setState({ isMarkerShown: false })
+//     this.delayedShowMarker()
+//   }
+
+//   render() {
+//     return (
+//       <MyMapComponent
+//         isMarkerShown={this.state.isMarkerShown}
+//         onMarkerClick={this.handleMarkerClick}
+//       />
+//     )
+//   }
+// }
