704188f7c1ecb02af5214cd03831fc18351bf9b7
Daniil_Sapielkin <vipdanik98@gmail.com>
2020-04-20 15:21:47 +0300

    add button

--- bigbrain/src/App.js
+++ bigbrain/src/App.js
@@ -2,9 +2,15 @@ import React from "react";
 import "./App.css";
 
 import { MainContainer } from "./container/MainContainer";
+import { ButtonPlus } from "./components/Button/ButtonMainPlus";
 
 function App() {
-  return <MainContainer />;
+  return (
+    <div>
+      <MainContainer />
+      <ButtonPlus />
+    </div>
+  );
 }
 
 export default App;
--- bigbrain/src/components/Button/ButtonMainPlus.jsx
+++ bigbrain/src/components/Button/ButtonMainPlus.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Button from "muicss/lib/react/button";
 
 import st from "./ButtonPlus.module.css";
 
 export function ButtonPlus() {
-  return <div className={st.buttonPlus}></div>;
+  return (
+    <div className={st.buttonPlus}>
+      <Button className={st.btn} variant="raised" color="primary">
+        <p>Report a problem</p>
+      </Button>
+    </div>
+  );
 }
