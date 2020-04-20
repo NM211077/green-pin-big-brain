be08ae4e3fff7b3860719cef45dd5f4a732ed65f
Daniil_Sapielkin <vipdanik98@gmail.com>
2020-04-20 14:51:30 +0300

    fix map

--- bigbrain/src/container/MapContainer.jsx
+++ bigbrain/src/container/MapContainer.jsx
@@ -3,19 +3,17 @@ import {
   withScriptjs,
   withGoogleMap,
   GoogleMap,
-  Marker,
 } from "react-google-maps";
 
 import { compose, withProps } from "recompose";
 
-/*API KEY AIzaSyBIHu3UtN5LFuO9rEQuFLaSAiStv6VB3Qs
- With apiKey not working 
-"https://maps.googleapis.com/maps/api/js?key=AIzaSyBIHu3UtN5LFuO9rEQuFLaSAiStv6VB3Qs=3.exp&libraries=geometry,drawing,places",*/
+/*API KEY AIzaSyBIHu3UtN5LFuO9rEQuFLaSAiStv6VB3Qs*/
 
 export const MyMapComponent = compose(
   withProps({
     googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?=3.exp&libraries=geometry,drawing,places",
+    "https://maps.googleapis.com/maps/api/js?v=weekly&key=AIzaSyBIHu3UtN5LFuO9rEQuFLaSAiStv6VB3Qs",
+
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `100vh` }} />,
     mapElement: <div style={{ height: `100%` }} />,
@@ -24,7 +22,7 @@ export const MyMapComponent = compose(
   withGoogleMap
 )((props) => (
   <GoogleMap
-    defaultZoom={13}
+    zoom={13}
     defaultCenter={{ lat: 49.990159, lng: 36.233081 }}
   />
 ));
