5a35fcab9276432eba8ad11479e9f5b0f2f70646
Daniil_Sapielkin <vipdanik98@gmail.com>
2020-04-27 02:29:44 +0300

    update havnar , add login

--- bigbrain/src/components/Navbar.jsx
+++ bigbrain/src/components/Navbar.jsx
@@ -2,11 +2,17 @@ import React, { Component } from "react";
 
 export function Navbar() {
   return (
-    <ul className="navbar">
-      <li>Home</li>
-      <li>Statistics</li>
-      <li>Volonteering</li>
-      <li>Log in</li>
-    </ul>
+    <div className="havbar">
+      <ul className="navbar-title">
+        <li>Home</li>
+        <li>Statistics</li>
+        <li>Volonteering</li>
+      </ul>
+      <div className="enter-login">
+        <div className="login">Log in</div>
+        <div className="create-login">CREATE ACCOUNT</div>
+      </div>
+    </div>
   );
 }
+
