5610883f899b4017777155cfbc6bb29ff007345d
Daniil_Sapielkin <vipdanik98@gmail.com>
2020-04-28 22:40:32 +0300

    first fix

--- bigbrain/src/App.js
+++ bigbrain/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import { MainContainer, HeaderContainer } from "./container/index.js";
+import "./assets/style/index.sass";
+import "./assets/icon/css/fontello.css";
 
 function App() {
   return (
