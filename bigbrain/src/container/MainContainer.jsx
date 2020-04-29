da97dc841907ff06d4798e0860d6e2f341f6c15e
Daniil_Sapielkin <vipdanik98@gmail.com>
2020-04-29 23:57:12 +0300

    fitst commit

--- bigbrain/src/container/MainContainer.jsx
+++ bigbrain/src/container/MainContainer.jsx
@@ -1,34 +1,32 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import { MapComponent, ButtonPlus } from "../components/index.js";
-import axios from 'axios';
 
-
-const API_URL = 'https://arcane-eyrie-30848.herokuapp.com';
+const API_URL =
+  "https://cors-anywhere.herokuapp.com/https://arcane-eyrie-30848.herokuapp.com";
 
 export class MainContainer extends Component {
-    state = {
-        pin: [],
-    }
+  state = {
+    pin: [],
+    showModal: false,
+  };
 
-    componentDidMount() {
-        const url = `${API_URL}/api/v1/`;
-        return axios.get(url)
-            .then(response => response.data)
-            .then(result => {console.log('result',result)
-                
-                this.setState({pin: result});
-            });
-    }
+  componentDidMount() {
+    const url = `${API_URL}/api/v1/pin`;
+    return axios
+      .get(url)
+      .then((response) => response.data)
+      .then((result) => this.setState({ pin: result }))
+      .catch((error) => console.log("error", error));
+  }
 
   render() {
-      const pin = this.state.pin;
-      console.log(pin);
+    const pin = this.state.pin;
+    console.log(pin);
     return (
       <div className="main">
-        <MapComponent
-            pin={pin}
-        />
+        <MapComponent pin={pin} />
         <ButtonPlus />
       </div>
     );
