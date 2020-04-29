62f81cb88145677a25bfb9d28daa819a00ae2a45
Nataly Moskalenko <y.natali.moskalenko@gmail.com>
2020-04-29 22:12:49 +0300

    Change api

--- bigbrain/src/components/ButtonMainPlus.jsx
+++ bigbrain/src/components/ButtonMainPlus.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Button from "muicss/lib/react/button";
 
-export function ButtonPlus() {
-  return (
+export function ButtonPlus(props) {
+    const{onClickBtn}=props;
+    return (
     <div className="btn-problem-block">
-      <Button className="btn-problem" variant="raised" color="primary">
+      <Button className="btn-problem" variant="raised" color="primary" onClick ={onClickBtn}>
         <p>Report a problem</p>
       </Button>
     </div>
