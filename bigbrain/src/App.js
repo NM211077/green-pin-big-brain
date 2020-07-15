214bf9e294caf32289c9681440338616eec1feb2
Nataly Moskalenko <y.natali.moskalenko@gmail.com>
2020-07-15 19:58:05 +0300

    corrected App

--- bigbrain/src/App.js
+++ bigbrain/src/App.js
@@ -1,22 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, } from "react";
 import {HeaderContainer} from "./container/index.js";
 import "./assets/style/index.sass";
 import "./assets/icon/css/fontello.css";
-import {BrowserRouter,Link} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import Routes from './router'
-import { LogInPage } from "./components/index.js"
-import store from "./redux/store";
-import { AppContext } from "./libs/contextLib";
+
 
 function App() {
 
     return (
 
         <BrowserRouter>
-            <>
+            <div>
                 <HeaderContainer />
                 <Routes/>
-            </>
+            </div>
         </BrowserRouter>
 
     );
